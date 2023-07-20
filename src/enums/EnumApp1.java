00ed609e204fa38f1dbde98f0564956d9a72fa1d
samuel <samugit.projects@gmail.com>
2023-07-20 15:58:23 -0300

    Aula14 Enum para representar os meses do ano

---
+++ src/enums/EnumApp1.java
@@ -0,0 +1,17 @@
+package enums;
+
+@SuppressWarnings("ALL")
+public class EnumApp1 {
+
+    enum Month { // enum é um tipo de dado
+
+        JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER
+    }
+
+    public static void main(String[] args) {
+
+        Month firstMonth = Month.JANUARY;
+        Month noMonth = null; // internamente é como se fosse uma classe por isso pode ter valor null
+        // e somente isso, qualquer outra coisa fora do que foi específicado no enum não vai compilar
+    }
+}
