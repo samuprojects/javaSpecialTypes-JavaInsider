d03787906df84579aa38b3f06b75c05c0b7ed8dd
samuel <samugit.projects@gmail.com>
2023-07-21 22:29:41 -0300

    Aula22 O que são as classes wrappers do Java

---
+++ src/theory/JavaWrapperClasses.java
@@ -0,0 +1,27 @@
+package theory;
+
+@SuppressWarnings("ALL")
+public class JavaWrapperClasses {
+
+    /*
+
+    Classes Wrappers são classes que representam os tipos no mundo dos objetos
+
+    Tipo Primitivo ==> Classe Wrapper
+        boolean ==> Boolean
+        byte ==> Byte
+        short ==> Short
+        char ==> Character
+        int ==> Integer
+        float ==> Float
+        long ==> Long
+        double ==> Double
+
+   Um objeto de uma classe wrapper envolve o tipo primitivo em questão
+   Wrappe em inglês é embrulho
+
+   Objetos de classes Wrappers são imutáveis
+
+     */
+
+}
